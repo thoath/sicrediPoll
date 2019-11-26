6a7630eef50b57be31e955dd264bc20813d68554
thoath <lucaskochluz@gmail.com>
2019-11-25 22:39:47 -0300

    commit

---
+++ src/test/java/br/com/sicredi/SicrediPollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediPollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
